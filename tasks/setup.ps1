50b49ce4a2d23f54a6ee5d535a9b2ad54ca5e060
Дмитрович Олег <doublefint@gmail.com>
2020-05-06 18:09:05 +0300

    fix

--- tasks/setup.ps1
+++ tasks/setup.ps1
@@ -1,12 +1,5 @@
-#npm install -g vulcanize ;old-version
-npm install -g polymer-bundler
-
-npm install -g crisper
-npm install
-cd src/
+yarn
+cd src
 bower install
-
-#OLD
-cd ../
-npm install -g grunt-cli
+cd ..
 grunt
--- tasks/setup.sh
+++ tasks/setup.sh
@@ -1,12 +1,9 @@
-sudo apt-get install npm
-sudo npm install -g bower
-sudo npm install -g vulcanize
-sudo npm install -g crisper
-npm install
+sudo apt-get install yarn
+yarn
+
+yarn global add bower
 cd src/
 bower install
 
-#OLD
 cd ../
-sudo npm install -g grunt-cli
 grunt
